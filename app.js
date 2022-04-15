27f4ec1fa9d93a591b2774abb7c9b8135934cd36
DevRavi <35264417+raviShrestha-js@users.noreply.github.com>
2022-04-16 08:55:48 +1000

    nav links active with scrolling

---
+++ app.js
@@ -0,0 +1,20 @@
+const sections = document.querySelectorAll("section");
+const navList = document.querySelectorAll("nav ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (window.pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+  console.log(current);
+
+  navList.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
